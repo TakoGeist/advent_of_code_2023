b06d09e1f8934972dc54bf6be2b735f30218f07c
TakoGeist <d.silva.ramos12@gmail.com>
2023-12-07 16:29:38 +0000

    Added day04

---
+++ day04/src/bin/part2.rs
@@ -0,0 +1,40 @@
+use std::collections::VecDeque;
+
+
+fn scratch(input: &str) -> u32{
+    input.replace("  ", " ").lines().map(|line| 
+            line.split(":").nth(1).unwrap().split_once("|").unwrap())
+         .map(|(left, right)| 
+            (left.trim().split(" ").collect::<Vec<_>>(), right.trim().split(" ")))
+         .map(|(x, y)| 
+            y.filter(move |elem| x.contains(elem)).collect::<Vec<_>>())
+         .fold((0, VecDeque::from(vec![1; 10])), |(acc, mut buffer), card| { 
+            buffer.push_back(1);
+            let x = buffer.pop_front().unwrap();
+            for i in 0..card.len(){
+                buffer[i] += x;
+            }
+            (acc + x, buffer)
+        }).0
+}
+
+fn main(){
+    let input = include_str!("..\\input.txt");
+    println!("Part2 -> {}", scratch(input))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let res ="Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(scratch(res), 30);
+    }
+}
