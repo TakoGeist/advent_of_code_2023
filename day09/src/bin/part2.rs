2fedf571331d8123da876f33a9ed3f00ab2876af
TakoGeist <d.silva.ramos12@gmail.com>
2023-12-09 11:11:08 +0000

    Added day09

---
+++ day09/src/bin/part2.rs
@@ -0,0 +1,34 @@
+
+fn mirage(input: &str) -> i32{
+    input.lines()
+        .map(|line| line.split(" ")
+        .filter_map(|x| x.parse::<i32>().ok())
+        .collect::<Vec<_>>())
+        .fold(0,|acc,mut line| {
+            line.reverse();
+            let mut val = 0;
+            while line.iter().any(|&x| x != 0){
+                (0..line.len() - 1).for_each(|idx| line[idx] = line[idx + 1] - line[idx]);
+                val += line.pop().unwrap();
+            }
+            acc + val
+        })
+}
+
+fn main(){
+    let input = include_str!("..\\input.txt");
+    println!("Part2 -> {}", mirage(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let res = "0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45";
+        assert_eq!(mirage(res), 2);
+    }
+}
